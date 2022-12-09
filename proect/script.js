2def5a53ded7136bb16939bf63cedda8c290e1a8
Grinchcreator <119819551+Grinchcreator@users.noreply.github.com>
2022-12-09 20:12:01 +0300

    Add files via upload

---
+++ proect/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(e) {
+  $("#pause").css("display","none");
+$("#play").click(function(e) {
+$("#play").css("display","none");
+$("#pause").css("display","block");
+});
+$("#pause").click(function(e) {
+$("#play").css("display","block");
+$("#pause").css("display","none");
+});
+});
